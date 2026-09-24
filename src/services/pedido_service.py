66e4a64d62b7467d0e498229c604e011ee926938
Lucas <lucaschoucate@gmail.com>
2026-09-24 10:29:38 -0300

    refactor: implementa arquitetura em camadas no fluxo de pedidos

--- src/services/pedido_service.py
+++ src/services/pedido_service.py
@@ -1,15 +1,17 @@
 from src.models.pedido import Pedido
 
+
 class PedidoService:
-    """Classe de servoço para processar pedido e aplicar descontos"""
+    """Classe de serviço para processar pedidos e aplicar descontos."""
 
-    def __init__(self):
-        self.pedidos=[]
+    def __init__(self, repository):
+        self.repository = repository
 
-    def adicionar_pedido(self,pedido:Pedido):
-        self.pedidos.append(pedido)
+    def adicionar_pedido(self, pedido: Pedido):
+        self.repository.adicionar_pedido(pedido)
 
-    def processar_pedido(self):
-        for pedido in self.pedidos:
+    def processar_pedidos(self):
+        pedidos = self.repository.listar_pedidos()
+        for pedido in pedidos:
             print(f"Cliente: {pedido.cliente}")
             print(f"Valor final: {pedido.valor_final(pedido.valor_original)}")
